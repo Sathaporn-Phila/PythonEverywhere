46b3bb8b011ff2e3a01f42be38b2729a40d7265a
Kantinun <s6201012630011@email.kmutnb.ac.th>
2021-02-20 20:16:16 +0700

    Add more model design

--- mysite/vocab/models.py
+++ mysite/vocab/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
-class Vocab(model.Model):
+class Vocab(models.Model):
     vocab_text = models.CharField(max_length=200)
 
     def __str__(self):
         return self.question_text #แสดงคำศัพท์
 
+class Definition(models.Model):
+    def_text = models.CharField(max_length=200)
+    vocab = models.ForeignKey(Vocab, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.question_text #แสดงคำศัพท์
