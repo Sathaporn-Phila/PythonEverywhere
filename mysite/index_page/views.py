d3d1f41265fc749fd1625cfafdb4e8a25c36b2ef
Sathaporn01 <nuinee_2544@hotmail.com>
2021-02-22 16:40:44 +0700

    Try to server

---
+++ mysite/index_page/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+# Create your views here.
+def index(request):
+    return render(request,"index_page/index.html")
